e38b95c57c59e419285cc5e597319f99b9b28c21
mat <matmccann@gmail.com>
2019-08-14 00:02:01 -0700

    bash script updates

--- ec2_run.sh
+++ ec2_run.sh
@@ -7,18 +7,19 @@ sudo apt-get update
 sudo apt-get install \
     python3-venv \
     npm \
+    nodejs \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common \
-    nginx
-echo ***********************
+    nginx 
+echo 
 echo
 
 echo download and verify docker depencies	
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo ***********************
+echo 
 echo verify fingerprints - docker
 sudo apt-key fingerprint 0EBFCD88
 
@@ -28,35 +29,36 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 echo
-echo ***********************
+echo 
 echo
 echo install docker   
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo
-echo ***********************
+echo 
 echo
 docker-compose --version
-echo ***********************
+echo 
 echo
 
 echo activate the python virtual environment
 cd ~/testdriven-app/services/users
 python3 -m venv venv
 source venv/bin/activate
+echo
 echo give r/x permission so docker-compose can execute 
 sudo chmod 755 entrypoint.sh
-echo ***********************
+echo 
 echo
 
 # install npm to create node_modules and package-lock.json
 cd ~/testdriven-app/services/cleanui
-echo ***********************
+echo 
 echo
 echo running npm run build
 npm run build 
-echo ***********************
+echo 
 echo
 
 
@@ -75,7 +77,7 @@ echo
 #sudo docker-compose up -d --build
 sudo docker-compose -f docker-compose-prod.yml up -d --build
 echo
-echo ***********************
+echo 
 echo backend http://ec2-18-216-81-201.us-east-2.compute.amazonaws.com:5001/users/ping
 echo
 echo frontend
